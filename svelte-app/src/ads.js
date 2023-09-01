16888acb1eaaf1c621b527013b34da87b275678e
vitroid <vitroid@gmail.com>
2023-09-01 23:03:50 +0900

    .

--- svelte-app/src/ads.js
+++ svelte-app/src/ads.js
@@ -3,8 +3,8 @@ export const ads = [
     "pdf": "pdf/ad_1.pdf",
     "sp": [
       {
-        "name": "アジレント・テクノロジー株式会社",
-        "url": "https://www.chem-agilent.com"
+        "name": "スペクトリス株式会社",
+        "url": "https://www.malvernpanalytical.com/jp"
       }
     ],
     "tn": "tn/ad_1.jpg"
@@ -13,8 +13,8 @@ export const ads = [
     "pdf": "pdf/ad_2.pdf",
     "sp": [
       {
-        "name": "株式会社アントンパール・ジャパン",
-        "url": "https://www.anton-paar.com/jp-jp/"
+        "name": "BCSJ Chem. Lett.",
+        "url": "https://www.journal.csj.jp/journal/cl"
       }
     ],
     "tn": "tn/ad_2.jpg"
@@ -23,8 +23,8 @@ export const ads = [
     "pdf": "pdf/ad_3.pdf",
     "sp": [
       {
-        "name": "株式会社セルシステム",
-        "url": "http://www.cellsystem.co.jp/"
+        "name": "株式会社ブライト",
+        "url": "https://bright-jp.com"
       }
     ],
     "tn": "tn/ad_3.jpg"
@@ -33,8 +33,8 @@ export const ads = [
     "pdf": "pdf/ad_4.pdf",
     "sp": [
       {
-        "name": "株式会社島津製作所",
-        "url": "https://www.shimadzu.co.jp"
+        "name": "ブルカージャパン株式会社",
+        "url": "https://www.bruker.com/ja.html"
       }
     ],
     "tn": "tn/ad_4.jpg"
@@ -43,8 +43,8 @@ export const ads = [
     "pdf": "pdf/ad_5.pdf",
     "sp": [
       {
-        "name": "株式会社東陽テクニカ",
-        "url": "https://www.toyo.co.jp"
+        "name": "マイクロトラック・ベル株式会社",
+        "url": "https://www.microtrac.com/jp/"
       }
     ],
     "tn": "tn/ad_5.jpg"
@@ -53,109 +53,69 @@ export const ads = [
     "pdf": "pdf/ad_6.pdf",
     "sp": [
       {
-        "name": "株式会社ブライト",
-        "url": "https://bright-jp.com"
+        "name": "株式会社ユニソク",
+        "url": "https://www.unisoku.co.jp"
       }
     ],
     "tn": "tn/ad_6.jpg"
   },
   {
-    "pdf": "pdf/ad_7.pdf",
+    "pdf": "pdf/ad_7_1.pdf",
     "sp": [
       {
-        "name": "株式会社UNICO",
-        "url": "https://www.glovebox.co.jp"
+        "name": "Royal Society of Chemistry",
+        "url": "https://rsc.li/Dalton"
       }
     ],
-    "tn": "tn/ad_7.jpg"
+    "tn": "tn/ad_7_1.jpg"
   },
   {
-    "pdf": "pdf/ad_8.pdf",
+    "pdf": "pdf/ad_7_2.pdf",
     "sp": [
       {
-        "name": "株式会社ユニソク",
-        "url": "https://www.unisoku.co.jp"
+        "name": "Royal Society of Chemistry",
+        "url": "https://rsc.li/frontiers-inorganic"
       }
     ],
-    "tn": "tn/ad_8.jpg"
+    "tn": "tn/ad_7_2.jpg"
   },
   {
-    "pdf": "pdf/ad_9.pdf",
+    "pdf": "pdf/ad_89.pdf",
+    "sp": [
+      {
+        "name": "HPCシステムズ株式会社",
+        "url": "https://www.hpc.co.jp"
+      },
+      {
+        "name": "日本分光株式会社",
+        "url": "http://www.jasco.co.jp/jpn/home/index.html"
+      }
+    ],
+    "tn": "tn/ad_89.jpg"
+  },
+  {
+    "pdf": "pdf/ad_1011.pdf",
+    "sp": [
+      {
+        "name": "ブルカージャパン株式会社",
+        "url": "https://www.bruker.com/ja.html"
+      },
+      {
+        "name": "有限会社ラブディポット",
+        "url": "https://www.google.com/maps/place/3-chōme-14-7+Yokokawa/@35.7053719,139.8121558,18.82z/data=!4m7!3m6!1s0x60188ed5454e1473:0xd0ab6fcd36d4730e!4b1!8m2!3d35.7054771!4d139.8132035!16s%2Fg%2F11clnn4b78?entry=ttu"
+      }
+    ],
+    "tn": "tn/ad_1011.jpg"
+  },
+  {
+    "pdf": "pdf/ad_12.pdf",
     "sp": [
       {
         "name": "株式会社リガク",
         "url": "https://japan.rigaku.com/ja"
       }
     ],
-    "tn": "tn/ad_9.jpg"
-  },
-  {
-    "pdf": "pdf/ad_10.pdf",
-    "sp": [
-      {
-        "name": "田中貴金属工業株式会社",
-        "url": "https://gold.tanaka.co.jp/index.php"
-      }
-    ],
-    "tn": "tn/ad_10.jpg"
-  },
-  {
-    "pdf": "pdf/ad_11.pdf",
-    "sp": [
-      {
-        "name": "福岡酸素株式会社",
-        "url": "https://fksanso.co.jp"
-      }
-    ],
-    "tn": "tn/ad_11.jpg"
-  },
-  {
-    "pdf": "pdf/ad_12.pdf",
-    "sp": [
-      {
-        "name": "ブルカージャパン株式会社",
-        "url": "https://www.bruker.com/ja.html"
-      }
-    ],
     "tn": "tn/ad_12.jpg"
-  },
-  {
-    "pdf": "pdf/ad_13.pdf",
-    "sp": [
-      {
-        "name": "マイクロトラック・ベル株式会社",
-        "url": "https://www.microtrac.com/jp/"
-      }
-    ],
-    "tn": "tn/ad_13.jpg"
-  },
-  {
-    "pdf": "pdf/ad_14.pdf",
-    "sp": [
-      {
-        "name": "株式会社新興精機",
-        "url": "https://shinkouseiki.co.jp"
-      },
-      {
-        "name": "桜木理化学機械株式会社",
-        "url": "http://www.sakuragi-rk.co.jp"
-      }
-    ],
-    "tn": "tn/ad_14.jpg"
-  },
-  {
-    "pdf": "pdf/ad_15.pdf",
-    "sp": [
-      {
-        "name": "三共出版株式会社",
-        "url": "https://www.sankyoshuppan.co.jp"
-      },
-      {
-        "name": "",
-        "url": ""
-      }
-    ],
-    "tn": "tn/ad_15.jpg"
   }
 ]
 ;
